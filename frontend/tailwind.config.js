ec7cdf99d3d13f2246d459f3dd80bbd7e8466407
Lee Hyunji <hzi284914@gmail.com>
2025-03-11 11:00:06 +0900

    :wrench: tailwind 추가

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}"], // 적용할 파일 경로 추가
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
