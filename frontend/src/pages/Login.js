d30b423f564f32b9332319f5bebedfc597831eb2
Lee Hyunji <hzi284914@gmail.com>
2025-03-12 16:53:26 +0900

    :beers: 세미콜론(;) 추가

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -3,7 +3,7 @@ import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 import { AuthContext } from "../context/AuthContext";
 
-const BASE_URL = process.env.REACT_APP_API_URL
+const BASE_URL = process.env.REACT_APP_API_URL;
 
 const Login = () => {
   const [username, setUsername] = useState("");
