b8d6ee14e366414894f4f7d1cd571334cad4dd74
bubpen <shghtjd711@naver.com>
2025-03-19 17:35:38 +0900

    :recycle: 이메일 형식, url 변경

--- steamate/config/settings.py
+++ steamate/config/settings.py
@@ -32,9 +32,9 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["52.79.104.109","steamate.co.kr","api.steamate.co.kr","localhost", "127.0.0.1","steamate-front.vercel.app"]
+ALLOWED_HOSTS = ["52.79.104.109","steamate","localhost", "127.0.0.1","steamate-front.vercel.app"]
 
-CSRF_TRUSTED_ORIGINS = ["https://steamate.co.kr","https://api.steamate.co.kr","http://52.79.104.109", "http://localhost", "http://steamate","https://steamate-front.vercel.app"]
+CSRF_TRUSTED_ORIGINS = ["http://52.79.104.109", "http://localhost", "http://steamate","https://steamate-front.vercel.app"]
 
 # Application definition
 
@@ -70,20 +70,10 @@ MIDDLEWARE = [
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
-    'http://52.79.104.109',
-    'https://www.steamate.co.kr',
-    'https://steamate.co.kr',
-    'https://api.steamate.co.kr',  
+    'http://52.79.104.109'  # React 앱의 주소
 ]
 
-
-
-# CORS_ALLOW_CREDENTIALS = True
-
-CORS_ALLOW_HEADERS = ["*"]
-
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
+CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = 'config.urls'
 
